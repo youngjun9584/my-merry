9bf8c31069c897fcc05ed5af61c750ee3de204a4
morgan-apart <morgan.park@apartmentary.com>
2025-10-20 00:49:32 +0900

    dfsd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -126,6 +126,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://openapi.map.naver.com" />
 
         {/* 카카오톡 SDK */}
+        {/* eslint-disable-next-line @next/next/no-sync-scripts */}
         <script
           src="https://t1.kakaocdn.net/kakao_js_sdk/2.7.6/kakao.min.js"
           integrity="sha384-WAtVcQYcmTO/N+C1N+1m6Gp8qxh+3NlnP7X1U7qP6P5dQY/MsRBNTh+e1ahJrkEm"
